e35accec80f350cc948b0265a1075c388ce714b6
mihail_sheludyakov <bvg2003@bk.ru>
2023-08-30 14:03:39 +0300

    Add resolveAndHandle methods

--- src/main/java/ru/michaelshell/sampo_bot/dispatcher/UpdateDispatcher.java
+++ src/main/java/ru/michaelshell/sampo_bot/dispatcher/UpdateDispatcher.java
@@ -33,7 +33,7 @@ public class UpdateDispatcher {
         if (message != null && message.hasText() && message.isUserMessage()) {
 
             if (!AuthUtils.isAuthenticated(session)) {
-                handlers.get(RegisterHandler.class).handleUpdate(update, session);
+                resolveAndHandleUpdate(RegisterHandler.class, update, session);
             }
 
             String messageText = message.getText();
@@ -44,83 +44,88 @@ public class UpdateDispatcher {
             waitingStatusMessageRouter(update, session);
 
             switch (messageText) {
-                case "/start", "/help" -> handlers.get(StartHandler.class).handleUpdate(update, session);
-                case "/promote" -> handlers.get(PromotionHandler.class).handleUpdate(update, session);
+                case "/start", "/help" -> resolveAndHandleUpdate(StartHandler.class, update, session);
+                case "/promote" -> resolveAndHandleUpdate(PromotionHandler.class, update, session);
                 case "/events", "Список коллективок" ->
-                        handlers.get(EventListHandler.class).handleUpdate(update, session);
-                case "Добавить" -> handlers.get(EventCreateHandler.class).handleUpdate(update, session);
-                case "/profile" -> handlers.get(EditProfileHandler.class).handleUpdate(update, session);
-                case "/all" -> handlers.get(NotifyAllHandler.class).handleUpdate(update, session);
+                        resolveAndHandleUpdate(EventListHandler.class, update, session);
+                case "Добавить" -> resolveAndHandleUpdate(EventCreateHandler.class, update, session);
+                case "/profile" -> resolveAndHandleUpdate(EditProfileHandler.class, update, session);
+                case "/all" -> resolveAndHandleUpdate(NotifyAllHandler.class, update, session);
             }
         }
 
-
         if (update.hasCallbackQuery()) {
             if (!AuthUtils.isAuthenticated(session)) {
-                handlers.get(RegisterHandler.class).handleCallback(update, session);
+                resolveAndHandleCallback(RegisterHandler.class, update, session);
             }
             String callbackData = update.getCallbackQuery().getData();
             switch (callbackData) {
                 case "buttonInfoYes", "buttonInfoNo" ->
-                        handlers.get(EventCreateHandler.class).handleCallback(update, session);
+                        resolveAndHandleCallback(EventCreateHandler.class, update, session);
                 case "buttonEventDelete", "buttonEventDeleteConfirmation" ->
-                        handlers.get(EventDeleteHandler.class).handleCallback(update, session);
-                case "buttonEventRegister" -> handlers.get(EventRegisterHandler.class).handleCallback(update, session);
+                        resolveAndHandleCallback(EventDeleteHandler.class, update, session);
+                case "buttonEventRegister" -> resolveAndHandleCallback(EventRegisterHandler.class, update, session);
                 case "buttonLeader", "buttonFollower" ->
-                        handlers.get(RoleSetHandler.class).handleCallback(update, session);
-                case "buttonSolo" -> handlers.get(EventSoloRegisterHandler.class).handleCallback(update, session);
-                case "buttonCouple" -> handlers.get(EventCoupleRegisterHandler.class).handleCallback(update, session);
+                        resolveAndHandleCallback(RoleSetHandler.class, update, session);
+                case "buttonSolo" -> resolveAndHandleCallback(EventSoloRegisterHandler.class, update, session);
+                case "buttonCouple" -> resolveAndHandleCallback(EventCoupleRegisterHandler.class, update, session);
                 case "buttonShowDancersList", "buttonRefresh" ->
-                        handlers.get(DancerListHandler.class).handleCallback(update, session);
+                        resolveAndHandleCallback(DancerListHandler.class, update, session);
                 case "buttonDeleteRegistration" ->
-                        handlers.get(DeleteEventRegistrationHandler.class).handleCallback(update, session);
-                case "buttonEventEdit" -> handlers.get(EventEditHandler.class).handleCallback(update, session);
+                        resolveAndHandleCallback(DeleteEventRegistrationHandler.class, update, session);
+                case "buttonEventEdit" -> resolveAndHandleCallback(EventEditHandler.class, update, session);
                 case "buttonEditEventTitle" ->
-                        handlers.get(EventEditTitleHandler.class).handleCallback(update, session);
-                case "buttonEditEventTime" -> handlers.get(EventEditTimeHandler.class).handleCallback(update, session);
-                case "buttonEditEventInfo" -> handlers.get(EventEditInfoHandler.class).handleCallback(update, session);
-                case "buttonSendEventInfo" -> handlers.get(SendEventInfoHandler.class).handleCallback(update, session);
+                        resolveAndHandleCallback(EventEditTitleHandler.class, update, session);
+                case "buttonEditEventTime" -> resolveAndHandleCallback(EventEditTimeHandler.class, update, session);
+                case "buttonEditEventInfo" -> resolveAndHandleCallback(EventEditInfoHandler.class, update, session);
+                case "buttonSendEventInfo" -> resolveAndHandleCallback(SendEventInfoHandler.class, update, session);
             }
         }
 
     }
-
 
     private void waitingStatusMessageRouter(Update update, Session session) {
 
         if (Boolean.TRUE.equals(session.getAttribute(EVENT_ADD_WAITING_FOR_INFO.name())) ||
                 Boolean.TRUE.equals(session.getAttribute(EVENT_ADD_WAITING_FOR_DATE.name())) ||
                 Boolean.TRUE.equals(session.getAttribute(EVENT_ADD_WAITING_FOR_NAME.name()))) {
-            handlers.get(EventCreateHandler.class).handleUpdate(update, session);
+            resolveAndHandleUpdate(EventCreateHandler.class, update, session);
         }
 
         if (Boolean.TRUE.equals(session.getAttribute(PROMOTION_WAITING_FOR_USERNAME.name()))) {
-            handlers.get(PromotionHandler.class).handleUpdate(update, session);
+            resolveAndHandleUpdate(PromotionHandler.class, update, session);
         }
 
         if (Boolean.TRUE.equals(session.getAttribute(SET_ROLE_WAITING_FOR_NAME.name()))) {
-            handlers.get(RoleSetHandler.class).handleUpdate(update, session);
+            resolveAndHandleUpdate(RoleSetHandler.class, update, session);
         }
 
         if (Boolean.TRUE.equals(session.getAttribute(COUPLE_REGISTER_WAITING_FOR_NAME.name()))) {
-            handlers.get(EventCoupleRegisterHandler.class).handleUpdate(update, session);
+            resolveAndHandleUpdate(EventCoupleRegisterHandler.class, update, session);
         }
 
         if (Boolean.TRUE.equals(session.getAttribute(EVENT_EDIT_WAITING_FOR_NAME.name()))) {
-            handlers.get(EventEditTitleHandler.class).handleUpdate(update, session);
+            resolveAndHandleUpdate(EventEditTitleHandler.class, update, session);
         }
 
         if (Boolean.TRUE.equals(session.getAttribute(EVENT_EDIT_WAITING_FOR_INFO.name()))) {
-            handlers.get(EventEditInfoHandler.class).handleUpdate(update, session);
+            resolveAndHandleUpdate(EventEditInfoHandler.class, update, session);
         }
 
         if (Boolean.TRUE.equals(session.getAttribute(EVENT_EDIT_WAITING_FOR_DATE.name()))) {
-            handlers.get(EventEditTimeHandler.class).handleUpdate(update, session);
+            resolveAndHandleUpdate(EventEditTimeHandler.class, update, session);
         }
 
         if (Boolean.TRUE.equals((session.getAttribute(NOTIFY_ALL.name())))) {
-            handlers.get(NotifyAllHandler.class).handleUpdate(update, session);
+            resolveAndHandleUpdate(NotifyAllHandler.class, update, session);
         }
+    }
 
+    private void resolveAndHandleUpdate(Class<? extends UpdateHandler> clazz, Update update, Session session) {
+        handlers.get(clazz).handleUpdate(update, session);
+    }
+
+    private void resolveAndHandleCallback(Class<? extends UpdateHandler> clazz, Update update, Session session) {
+        handlers.get(clazz).handleCallback(update, session);
     }
 }
