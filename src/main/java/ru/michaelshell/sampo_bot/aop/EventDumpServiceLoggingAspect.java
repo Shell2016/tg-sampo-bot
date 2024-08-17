c4c71869906919fe13b41b3c4579b6674042bfc3
Shell2016 <bvg2003@bk.ru>
2024-08-17 22:30:29 +0300

    Logging: * Add Loki4j appender * Add loki and grafana containers * Add logging for success dump

---
+++ src/main/java/ru/michaelshell/sampo_bot/aop/EventDumpServiceLoggingAspect.java
@@ -0,0 +1,22 @@
+package ru.michaelshell.sampo_bot.aop;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Aspect
+@Component
+public class EventDumpServiceLoggingAspect {
+
+    @Pointcut("execution(void ru.michaelshell.sampo_bot.service.EventDumpService.dumpEvents())")
+    public void dumpEvents() {
+    }
+
+    @AfterReturning("dumpEvents()")
+    public void eventDumpLogging() {
+        log.info("Successfully dumped to google spreadsheets");
+    }
+}
